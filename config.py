50c857db7767ec1bd558d4ebf66d7167508fad61
Akiful33 <saketalam725@gmail.com>
2024-07-22 11:18:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7056799783:AAFCKUfU68tv44pXzD2Dz0uzh8ZKvH_U_xA")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Kingboyarif")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6747320841")
 
 
 
